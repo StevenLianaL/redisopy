0da89dc513d54fb14b0ec3828f3f4483c03d7487
wz <wangzhou8284@gmail.com>
2022-12-05 15:17:02 +0800

    add redis set

--- redisopy/containers/base.py
+++ redisopy/containers/base.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 
+from redis.exceptions import ResponseError
+
 from redisopy.base.mixin import ConnMixin
 
 
@@ -9,7 +11,12 @@ class RedisBaseContainer:
 
     def __init__(self, key: str):
         self.key = key
-        self.init_redis_value()
+        try:
+            self.init_redis_value()
+        except ResponseError as e:
+            if "WRONGTYPE" in e.args[0]:
+                raise TypeError(f"Key {self.key} is not a {self.__class__.__name__}")
+            raise e
 
     @abstractmethod
     def init_redis_value(self):
